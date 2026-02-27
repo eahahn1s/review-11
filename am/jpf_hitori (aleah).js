4311614c06f0d27f1175c79570e082a74439129b
Elizanna <eahahn1s@capetigers.com>
2026-02-27 10:12:23 -0600

    fixed code

--- am/jpf_hitori (aleah).js
+++ am/jpf_hitori (aleah).js
@@ -5,8 +5,8 @@
    Tutorial 11
    Review Assignment
 
-   Author: Elizanna Hahn
-   Date:   
+   Author: Aleyah Perez
+   Date:   2/17/26
 
    Global Variables
    ================
@@ -46,13 +46,16 @@
       the numbers, blocks, and rating parameters.
 	
 */
-var allCells;
 
+let allCells;
+
+// Call the startUp function on page load
 window.onload = startUp;
 
 function startUp() {
-   document.getElementById("puzzleTitle").innerHTML = "Puzzle 1";
-document.getElementById("puzzle").innerHTML = drawHitori(hitori1Numbers, hitori1Blocks, hitori1Rating);
+    // Draw the first puzzle on the screen
+    document.getElementById("puzzleTitle").innerHTML = "Puzzle 1";
+    document.getElementById("puzzle").innerHTML = drawHitori(hitori1Numbers, hitori1Blocks, hitori1Rating);
 
     let puzzleButtons = document.getElementsByClassName("puzzles");
 
@@ -70,37 +73,34 @@ document.getElementById("puzzle").innerHTML = drawHitori(hitori1Numbers, hitori1
 }
 
 function switchPuzzle(e) {
-   if (confirm("Are you sure you want to switch puzzles? Your work will be lost.")) {
-   var puzzleID = e.target.id;
+    // Ask the user for confirmation to change the puzzle
+    if (confirm("Are you sure you want to switch puzzles? Your work will be lost.")) {
+        const puzzleID = e.target.id;
 
-   
-   document.getElementById("puzzleTitle").innerHTML = e.target.value;
+        document.getElementById("puzzleTitle").innerHTML = e.target.value;
 
-   switch (puzzleID) {
-      case "puzzle1":
-         document.getElementById("puzzle").innerHTML = 
-          drawHitori(hitori1Numbers, hitori1Blocks, hitori1Rating);
-          setupPuzzle();
-         break;
-      case "puzzle2":
-      document.getElementById("puzzle").innerHTML =
-         drawHitori(hitori2Numbers, hitori2Blocks, hitori2Rating);
-         setupPuzzle();
-      break;
-   case "puzzle3":
-      document.getElementById("puzzle").innerHTML =
-         drawHitori(hitori3Numbers, hitori3Blocks, hitori3Rating);
-         setupPuzzle();
-      break;
-   }
-
-   }
+        // Change the puzzle based on which button they clicked
+        switch (puzzleID) {
+            case "puzzle1":
+                document.getElementById("puzzle").innerHTML = drawHitori(hitori1Numbers, hitori1Blocks, hitori1Rating);
+                setupPuzzle();
+                break;
+            case "puzzle2":
+                document.getElementById("puzzle").innerHTML = drawHitori(hitori2Numbers, hitori2Blocks, hitori2Rating);
+                setupPuzzle();
+                break;
+            case "puzzle3":
+                document.getElementById("puzzle").innerHTML = drawHitori(hitori3Numbers, hitori3Blocks, hitori3Rating);
+                setupPuzzle();
+                break;
+        }
+    }
 }
 
 function setupPuzzle() {
-   allCells = document.querySelectorAll("td");
+    allCells = document.querySelectorAll("td");
 
-    
+    // Set all the puzzle tiles to their default white state
     allCells.forEach((cell) => {
         // Change the background color to white
         cell.style.backgroundColor = 'white';
@@ -109,7 +109,7 @@ function setupPuzzle() {
         // Change the border radius to 0%
         cell.style.borderRadius = '0';
 
-       
+        // Detect if the user clicks on a tile while pressing Shift, Alt, or neither
         cell.addEventListener('mousedown', (e) => {
             if (e.shiftKey) {
 
@@ -163,84 +163,79 @@ function findErrors() {
     }, 1000)
 }
 
-
-
-
-
-
-         
 /* ================================================================= */
 
 function checkSolution() {
-   /* Set the initial solved state of the puzzle to true */
-   var solved = true;
+    /* Set the initial solved state of the puzzle to true */
+    let solved = true;
 
-   /* Loop through the puzzle cells, exiting when an incorrect
-      cell is found, setting the solved variable to false */
+    /* Loop through the puzzle cells, exiting when an incorrect
+       cell is found, setting the solved variable to false */
 
-   for (var i = 0; i < allCells.length; i++) {
-      var cellColor = allCells[i].style.backgroundColor;
-      var cellClass = allCells[i].className;
+    for (let i = 0; i < allCells.length; i++) {
+        const cellColor = allCells[i].style.backgroundColor;
+        const cellClass = allCells[i].className;
 
-      /* A cell is incorrect if it is in the block class and is not black
-         or in the circle class and is not white */
-      if ( (cellClass == "blocks" && cellColor !== "black") || 
-           (cellClass == "circles" && cellColor !== "rgb(101, 101, 101)")) {
-         solved = false;
-         break;
-      }
-   }
+        /* A cell is incorrect if it is in the block class and is not black
+           or in the circle class and is not white */
+        if ((cellClass === "blocks" && cellColor !== "rgb(0, 0, 0)") ||
+            (cellClass === "circles" && cellColor !== "rgb(101, 101, 101)")) {
+            solved = false;
+            break;
+        }
+    }
 
-   /* If solved is still true after the loop, display an alert box */
-   if (solved) alert("Congratulations! You solved the puzzle!");
+    /* If solved is still true after the loop, display an alert box */
+    if (solved) alert("Congratulations! You solved the puzzle!");
 }
 
-function showSolution () {
-   for (var i = 0; i < allCells.length; i++) {
-      allCells[i].style.color = "";
-      allCells[i].style.backgroundColor = "";
-      allCells[i].style.borderRadius = "";
-   };   
+function showSolution() {
+    for (let i = 0; i < allCells.length; i++) {
+        allCells[i].style.color = "";
+        allCells[i].style.backgroundColor = "";
+        allCells[i].style.borderRadius = "";
+    }
+
 }
 
 function drawHitori(numbers, blocks, rating) {
 
-   /* Initial HTML String for the Hitori Puzzle */
-   var htmlString = "";
+    /* Initial HTML String for the Hitori Puzzle */
+    let htmlString;
 
-   /* numbers is a multidimensional array containing the
-      Hitori numbers; blocks is a corresponding 
-      multidimensional array containing the location of the
-      blocks which are indicated by the # character.
-      Non-blocking cells are indicated by a blank character.
-  */
+    /* Numbers is a multidimensional array containing the
+       Hitori numbers; blocks is a corresponding
+       multidimensional array containing the location of the
+       blocks which are indicated by the # character.
+       Non-blocking cells are indicated by a blank character.
+   */
 
-   /* Create a Web table with the id, hitoriGrid, containing
-      the numeric values. Blocks cells have the class name,
-      blocks. Non-blocking cells have the class name, circles
-  */
+    /* Create a Web table with the id, hitoriGrid, containing
+       the numeric values. Blocks cells have the class name,
+       blocks. Non-blocking cells have the class name, circles
+   */
 
-   var totalRows = numbers.length;
-   var totalCols = numbers[0].length;
-   htmlString = "<table id='hitoriGrid'>";
-   htmlString += "<caption>" + rating + "</caption>";
-   
+    const totalRows = numbers.length;
+    const totalCols = numbers[0].length;
+    htmlString = "<table id='hitoriGrid'>";
+    htmlString += "<caption>" + rating + "</caption>";
 
-   for (var i = 0; i < totalRows; i++) {
-      htmlString += "<tr>";
 
-      for (var j = 0; j < totalCols; j++) {
-         if (blocks[i][j] == "#") htmlString += "<td  class='blocks'>"
-         else htmlString += "<td class='circles'>";
+    for (let i = 0; i < totalRows; i++) {
+        htmlString += "<tr>";
 
-         htmlString += numbers[i][j];
-         htmlString +="</td>";
-      }
+        for (let j = 0; j < totalCols; j++) {
+            if (blocks[i][j] === "#") htmlString += "<td  class='blocks'>"
+            else htmlString += "<td class='circles'>";
 
-      htmlString += "</tr>";
-   }
+            htmlString += numbers[i][j];
+            htmlString += "</td>";
+        }
 
-   htmlString += "</table>";
+        htmlString += "</tr>";
+    }
 
-   return htmlString;
+    htmlString += "</table>";
+
+    return htmlString;
 }
